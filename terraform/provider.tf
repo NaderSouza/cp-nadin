a23dade4a86a7344e07242879075843cb2c72e3a
Nader Hadad Souza <naderhadadsouza@gmail.com>
2023-08-30 06:27:25 -0300

    acessa ai acessa ai

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,9 +16,9 @@ terraform {
   }
 }
 provider "aws" {
-  region = var.aws_region
-  shared_config_files      = ["./.aws/config"]
-  shared_credentials_files = ["./.aws/credentials"]
+  region                   = "us-east-1"
+  shared_config_files      = ["/.aws/config"]
+  shared_credentials_files = ["/.aws/credentials"]
   profile                  = "default"
-  }
+}
 
