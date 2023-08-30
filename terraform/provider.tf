34bcb219120404aee361d8e57750c9b62ad073b5
Nader Hadad Souza <naderhadadsouza@gmail.com>
2023-08-30 05:28:53 -0300

    acessa ai acessa ai

---
+++ terraform/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-state-nadin"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock-nadin"
+    profile        = "default"
+
+  }
+}
+provider "aws" {
+  region = var.aws_region
+  shared_config_files      = ["./.aws/config"]
+  shared_credentials_files = ["./.aws/credentials"]
+  profile                  = "default"
+  }
+
